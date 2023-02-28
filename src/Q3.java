d73c0bf1653af87fa35d2da1fea5addf657ec2d4
isabellaarg <113070363+isabellaarg@users.noreply.github.com>
2023-02-28 20:32:31 -0300

    Update Q3.java

--- src/Q3.java
+++ src/Q3.java
@@ -9,15 +9,17 @@ public class Q3 {
         System.out.println("Digite uma frase: ");
         frase = sc.nextLine();
 
-        for(int i=0; i<frase.length(); i++){
-            char f = frase.charAt(i);
-            if (f == 'a' || f == 'e' || f == 'i' || f == 'o' || f == 'u'){
+        //for(int i=0; i<frase.length(); i++){ Codigo feito por mim
+           // if (f == 'a' || f == 'e' || f == 'i' || f == 'o' || f == 'u'){
+               // vogaisT++;
+           // }
+        //}
+        
+        for(int i=0; i<frase.length(); i++){ //Codigo feito pela professora
+            if(frase.charAt(i) == 'a' || frase.charAt(i) == 'e' || frase.charAt(i) == 'i' || frase.charAt(i) == 'o'|| frase.charAt(i) == 'u' || frase.charAt(i) == 'A' || frase.charAt(i) == 'E' || frase.charAt(i) == 'I' || frase.charAt(i) == 'O'|| frase.charAt(i) == 'U')
                 vogaisT++;
-            }
         }
         System.out.println("Total de vogais: " + vogaisT);
-
-
-
     }
 }
+
